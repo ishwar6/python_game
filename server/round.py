d08ab97a0fc25b78ba726b3516e7fed5dd42eaed
ishwar6 <iisudrj11@gmail.com>
2022-08-06 01:54:14 +0530

    Guess Pattern

--- server/round.py
+++ server/round.py
@@ -1,5 +1,13 @@
 class Round(object):
-    def __init__(self, word, player_drawing) -> None:
+    def __init__(self, word, player_drawing, player) -> None:
+        """init object
+
+        Args:
+            word (str): str
+            player_drawing (Drawing): Player
+            players(list): Player[]
+
+        """
         self.word = word
         self.player_drawing = player_drawing
         self.player_guesses = []
@@ -7,6 +15,15 @@ class Round(object):
         self.player_scores = {player:0 for player in player_drawing}
 
     def guess(self, player, wrd):
+        """for guess by player
+
+        Args:
+            player (Player): Player
+            wrd (String): Guess Word
+
+        Returns:
+            bool: If player got correct answer
+        """
         return wrd == self.word
 
     
