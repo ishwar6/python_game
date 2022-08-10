c719eb9add8933700fddb6fe8d79107faa56d268
ishwar6 <iisudrj11@gmail.com>
2022-08-11 01:45:02 +0530

    Round

--- server/round.py
+++ server/round.py
@@ -1,18 +1,38 @@
+"""
+Round:
+        word: str
+        time: float
+        player_drawing: Player
+        player_guessed: list
+        skips: int
+        player_scores: list
+"""
+import time
+from _thread import *
+
 class Round(object):
     def __init__(self, word, player_drawing, player) -> None:
         """init object
 
         Args:
             word (str): str
-            player_drawing (Drawing): Player
+            player_drawing (Drawing): Player Object
             players(list): Player[]
 
         """
         self.word = word
         self.player_drawing = player_drawing
-        self.player_guesses = []
+        self.player_guessed = []
         self.skips = 0
         self.player_scores = {player:0 for player in player_drawing}
+        # self.start = time.time()
+        start_new_thread(self.time_thread, ())
+
+    def time_thread(self):
+        while self.time > 0:
+            time.sleep(1)
+            self.time -=1
+        self.end_round("Time is Up")
 
     def guess(self, player, wrd):
         """for guess by player
@@ -24,7 +44,34 @@ class Round(object):
         Returns:
             bool: If player got correct answer
         """
-        return wrd == self.word
+        # return wrd == self.word
+        correct = wrd = self.word
+        if correct:
+            self.player_guessed.append(player)
+
+
+    def player_left(self, player):
+        """removes the player from list and scores
+
+        Args:
+            player (Player): Player
+        Returns:
+            None
+
+        """
+        if player in self.player_scores:
+            del self.player_scores[player]
+        
+        if player in self.player_guessed:
+            self.player_guessed.remove(player)
+        
+        if player==self.player_drawing:
+            self.end_round("Drawing Player Left")
+
+    
+    def end_round(self, msg):
+        pass
+        
 
     
 
